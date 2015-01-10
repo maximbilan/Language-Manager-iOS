8ba31003209b9394ba1517715f5c4151224bce3d
Maxim Bilan <maximb.mail@gmail.com>
2015-01-10 13:53:59 +0200

    Modified project structure Implemented NSBundle category

---
+++ ios_language_manager/Sources/NSBundle+Language.h
@@ -0,0 +1,15 @@
+//
+//  NSBundle+Language.h
+//  ios_language_manager
+//
+//  Created by Maxim Bilan on 1/10/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSBundle (Language)
+
++ (void)setLanguage:(NSString *)language;
+
+@end
---
+++ ios_language_manager/Sources/NSBundle+Language.m
@@ -0,0 +1,45 @@
+//
+//  NSBundle+Language.m
+//  ios_language_manager
+//
+//  Created by Maxim Bilan on 1/10/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+#import "NSBundle+Language.h"
+#import <objc/runtime.h>
+
+static const char kBundleKey = 0;
+
+@interface BundleEx : NSBundle
+
+@end
+
+@implementation BundleEx
+
+- (NSString *)localizedStringForKey:(NSString *)key value:(NSString *)value table:(NSString *)tableName
+{
+    NSBundle *bundle = objc_getAssociatedObject(self, &kBundleKey);
+    if (bundle) {
+        return [bundle localizedStringForKey:key value:value table:tableName];
+    }
+    else {
+        return [super localizedStringForKey:key value:value table:tableName];
+    }
+}
+
+@end
+
+@implementation NSBundle (Language)
+
++ (void)setLanguage:(NSString *)language
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        object_setClass([NSBundle mainBundle],[BundleEx class]);
+    });
+    id value = language ? [NSBundle bundleWithPath:[[NSBundle mainBundle] pathForResource:language ofType:@"lproj"]] : nil;
+    objc_setAssociatedObject([NSBundle mainBundle], &kBundleKey, value, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+@end
